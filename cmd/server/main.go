7545816f17eb34b89336c71f1bab5d1dea2b7e33
noch-g <ladle.doings_0g@icloud.com>
2025-02-23 06:51:51 +0100

    feat: add connection to rabbitmq

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal(fmt.Errorf("could not connect to %s", connectionString))
+	}
+	defer conn.Close()
+	fmt.Printf("connection to RabbitMQ was successful\n")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Printf("\nShutting down connection\n")
 }
